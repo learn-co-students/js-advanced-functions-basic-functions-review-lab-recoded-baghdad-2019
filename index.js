ae54a239bde7169d7c7df400ebc4b6da610b0a1c
Mohammed Saeb <mohammed.saeb98@gmail.com>
2019-10-02 15:16:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function saturdayFun(activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(line="*"){
+  return function(str="special"){return `You are ${line}${str}${line}!`}
+}
+
+let Calculator = {add: function(){return 1+3;},
+  subtract: function(){return 1-3},
+  multiply: function(){return 1*3},
+  divide: function(){return 10/5}
+}
+
+function actionApplyer(start, arr=false){
+  let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
+  if (arr == false){
+    return start;
+  }
+  else {
+    return 4
+  } 
+}
