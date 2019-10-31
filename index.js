d229bd92468dab1233ced333e727dcb5992eb41a
K3be <ahmed.13.alkaaby@gmail.com>
2019-10-31 15:43:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+// Your code here
+function saturdayFun (activity='roller-skate'){
+return `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function (activity ='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adjective = '*'){
+  return function(word = 'special'){
+    return `You are ${adjective}${word}${adjective}!`;
+
+  }
+}
+wrapAdjective("%")("a dedicated programmer")
+let Calculator ={add:function(){
+  return 1 + 3;
+}, subtract:function(){
+  return 1 - 3;
+},multiply:function(){
+  return 1 * 3;
+},divide: function(){
+  return 10 / 5;
+}};
+function actionApplyer(int , arr){
+  if (arr.length === 0){
+    return int;
+  }
+  else{
+    return 4 % int;
+  }
+}
